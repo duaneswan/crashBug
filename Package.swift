436bf4fa25186683e77dbc8411f02cc171b2382a
D-Unit <duane@dtechmediacreations.com>
2024-11-20 16:15:44 -0600

    Add files via upload
    Added Swift Package Dependency

---
+++ Package.swift
@@ -0,0 +1,29 @@
+
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "crashBug",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "crashBug",
+            targets: ["crashBug"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "crashBug",
+            dependencies: [],
+            resources: [
+                .process("Resources")
+            ]
+        ),
+        .testTarget(
+            name: "crashBugTests",
+            dependencies: ["crashBug"]
+        ),
+    ]
+)
